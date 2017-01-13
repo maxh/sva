ea927cde56472b7bfe77fbc010baf50611e089a3
Max Heinritz <maxheinritz@gmail.com>
2017-01-12 16:29:03 -0800

    minor auth flow optimization;

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -1,32 +1,36 @@
 import { GoogleSignin } from 'react-native-google-signin';
 
+import * as types from '../actions/types';
+
 const INITIAL_SCOPES = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email'
 ];
 
-GoogleSignin.configure({
+const authLibPromise = GoogleSignin.configure({
   scopes: INITIAL_SCOPES,
   iosClientId: '447160699625-5rv22eleubev18po80835dbk33k190fk.apps.googleusercontent.com',
   webClientId: '447160699625-cuvgvmtcfpl1c1jehq9dntcd1sgomi3g.apps.googleusercontent.com',
   offlineAccess: true
-})
-.then(() => {
-  // you can now call currentUserAsync()
 });
 
+const getCurrentUserAsync = () => {
+  return authLibPromise.then(() => GoogleSignin.currentUserAsync())
+}
 
 export const signInGoogleUser = () => {
   return (dispatch, getState) => {
-    dispatch({type: 'GOOGLE_USER_REQUEST'});
-    return GoogleSignin.currentUserAsync().then(googleUser => {
+    dispatch({
+      type: types.GOOGLE_USER_REQUEST
+    });
+    return getCurrentUserAsync().then(googleUser => {
       dispatch({
-        type: 'GOOGLE_USER_SUCCESS',
+        type: types.GOOGLE_USER_SUCCESS,
         value: googleUser
       });
     }).catch(error => {
       dispatch({
-        type: 'GOOGLE_USER_FAILURE',
+        type: types.GOOGLE_USER_FAILURE,
         error: error
       });
     })
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,21 +1,23 @@
 import { combineReducers } from 'redux';
 
+import * as types from '../actions/types';
+
 
 function googleUser(state = {isLoading: false}, action) {
   switch (action.type) {
-    case 'GOOGLE_USER_REQUEST':
+    case types.GOOGLE_USER_REQUEST:
       return {
         isLoading: true,
         value: state.value,
         error: undefined
       };
-    case 'GOOGLE_USER_SUCCESS':
+    case types.GOOGLE_USER_SUCCESS:
       return {
         isLoading: false,
         value: action.value,
         error: undefined
       };
-    case 'GOOGLE_USER_FAILURE':
+    case types.GOOGLE_USER_FAILURE:
       return {
         isLoading: false,
         value: undefined,
