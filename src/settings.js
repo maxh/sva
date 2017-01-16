9cac2c3fe1c34b2e6b4e257c7e6678dcad7bb70c
Max Heinritz <maxheinritz@gmail.com>
2017-01-15 17:13:09 -0800

    add settings

---
+++ src/settings.js
@@ -0,0 +1,32 @@
+const settings = {};
+
+if (__DEV__) {
+  settings.port = 5000;
+  settings.urls = {
+    mainServer: 'http://localhost:' + settings.port,
+    binaryServer: 'ws://localhost:' + settings.port
+  }
+} else {
+  // TODO(max): Use prod here when it's ready.
+  const host = 'scout-loftboxlabs-staging.herokuapp.com';
+  settings.urls = {
+    mainServer: 'https://' + host,
+    binaryServer: 'wss://' + host
+  }
+}
+
+// Change these during development to purge the store upon app refresh.
+settings.dev = {
+  purgeStore: false,
+  forceGoogleSignOut: false
+};
+
+// Safety check to prevent misconfiguration in production.
+if (!__DEV__) {
+  settings.dev = {
+    purgeStore: false,
+    forceGoogleSignOut: false
+  };
+}
+
+export default settings;
