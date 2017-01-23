f9fce892b5d44c2c391d81a961741c817bef34e5
Max Heinritz <maxheinritz@gmail.com>
2017-01-23 09:17:36 -0800

    fix lint in SignInScreen.test.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,9 @@ module.exports = {
     "__DEV__": true,
     "fetch": true,
     "Headers": true,
-    "WebSocket": true
+    "WebSocket": true,
+    "expect": true,
+    "jest": true,
+    "it": true,
   }
 };
--- src/screens/SignInScreen.test.js
+++ src/screens/SignInScreen.test.js
@@ -9,12 +9,10 @@ jest.mock('react-native-google-signin', () => 'signin');
 it('renders correctly when not loading', () => {
   const props = {
     signIn: jest.fn(),
-    isLoading: false
+    isLoading: false,
   };
 
-  const tree = shallow(
-    <SignInScreen {...props} />
-  );
+  const tree = shallow(<SignInScreen {...props} />);
 
   expect(tree).toMatchSnapshot();
 });
@@ -23,12 +21,10 @@ it('renders correctly when not loading', () => {
 it('renders correctly when loading', () => {
   const props = {
     signIn: jest.fn(),
-    isLoading: true
+    isLoading: true,
   };
 
-  const tree = shallow(
-    <SignInScreen {...props} />
-  );
+  const tree = shallow(<SignInScreen {...props} />);
 
   expect(tree).toMatchSnapshot();
 });
